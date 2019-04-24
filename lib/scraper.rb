0c89acb027e13b32915b315e6efcef5ff4e6a4e7
Paige Wiener <snowplushie@yahoo.com>
2019-04-24 00:45:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,17 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogir::HTML(open(index_url))
+    student = []
+    index_page.css(".roster-cards-container").each do |student|
+      student.css(".student-card a").each do |info|
+        student_name = info.css(".student-name").text
+        student_location = info.css(".student-location").text
+        student_profile = "#{info.attr('href')}"
+        student << {:name => student_name, :location => student_location, :profile_url => student_profile}
+      end
+    end
+    student
   end
 
   def self.scrape_profile_page(profile_url)
